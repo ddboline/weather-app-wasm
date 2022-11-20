73000d5a01acff7de19e77aa3ad9e7d0cd52a68a
Daniel Boline <ddboline@gmail.com>
2022-11-20 11:50:49 -0500

    store history to local storage

--- weather-index/src/main.rs
+++ weather-index/src/main.rs
@@ -6,6 +6,8 @@ use dioxus::prelude::{
 };
 use fermi::{use_read, use_set, Atom};
 use golde::{call, init_app, trigger, App, Trigger};
+use wasm_bindgen::JsValue;
+use web_sys::window;
 
 #[cfg(debug_assertions)]
 static BASE_URL: &str = "https://www.ddboline.net";
@@ -28,8 +30,10 @@ fn app(cx: Scope) -> Element {
     let (url_path, set_url_path) = use_state(&cx, || "weather/plot.html").split();
     let (draft, set_draft) = use_state(&cx, String::new).split();
     let (current_loc, set_current_loc) = use_state(&cx, String::new).split();
-    let (search_history, set_search_history) =
-        use_state(&cx, || vec![String::from("zip=10001")]).split();
+    let (search_history, set_search_history) = use_state(&cx, || {
+        get_history().unwrap_or_else(|_| vec![String::from("zip=10001")])
+    })
+    .split();
 
     init_app(&cx, |_| {});
 
@@ -48,13 +52,16 @@ fn app(cx: Scope) -> Element {
         } else {
             format!("q={s}")
         };
-        set_search_history.modify(|sh| {
-            let mut v: Vec<String> = sh.iter().filter(|x| x.as_str() != loc).cloned().collect();
-            v.push(loc.clone());
-            v
-        });
-        set_search_history.needs_update();
-        set_location(loc);
+        if !search_history.contains(&loc) {
+            set_search_history.modify(|sh| {
+                let mut v: Vec<String> = sh.iter().filter(|x| x.as_str() != loc).cloned().collect();
+                v.push(loc.clone());
+                set_history(&v).expect("Failed to set history");
+                v
+            });
+            set_search_history.needs_update();
+            set_location(loc);
+        }
     }
 
     cx.render(rsx! {
@@ -125,6 +132,7 @@ fn app(cx: Scope) -> Element {
                                 set_search_history.modify(|sh| {
                                     let mut v: Vec<String> = sh.iter().filter(|x| x.as_str() != loc).cloned().collect();
                                     v.push(loc.clone());
+                                    set_history(&v).expect("Failed to set history");
                                     v
                                 });
                                 set_search_history.needs_update();
@@ -144,6 +152,7 @@ fn app(cx: Scope) -> Element {
                         set_search_history.modify(|sh| {
                             let mut v: Vec<String> = sh.iter().filter(|x| x.as_str() != s).cloned().collect();
                             v.push(s.into());
+                            set_history(&v).expect("Failed to set history");
                             v
                         });
                         set_search_history.needs_update();
@@ -173,4 +182,31 @@ fn app(cx: Scope) -> Element {
             }
         }
     })
+}
+
+fn set_history(history: &[String]) -> Result<(), JsValue> {
+    let window = window().ok_or_else(|| JsValue::from_str("No window"))?;
+    let local_storage = window
+        .local_storage()?
+        .ok_or_else(|| JsValue::from_str("No local storage"))?;
+    let history_str = serde_json::to_string(history).map_err(|e| {
+        let e: JsValue = format!("{e}").into();
+        e
+    })?;
+    local_storage.set_item("history", &history_str)?;
+    Ok(())
+}
+
+fn get_history() -> Result<Vec<String>, JsValue> {
+    let window = window().ok_or_else(|| JsValue::from_str("No window"))?;
+    let local_storage = window
+        .local_storage()?
+        .ok_or_else(|| JsValue::from_str("No local storage"))?;
+    match local_storage.get_item("history")? {
+        Some(s) => serde_json::from_str(&s).map_err(|e| {
+            let e: JsValue = format!("{e}").into();
+            e
+        }),
+        None => Ok(vec![String::from("zip=10001")]),
+    }
 }
